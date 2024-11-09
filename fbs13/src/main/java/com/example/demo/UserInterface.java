15d3744a6dea0cccc4a36289f8ce184bb4e71e03
balajee-rm <69756772+balajee-rm@users.noreply.github.com>
2024-11-09 07:16:22 +0530

    Delete fbs13 directory

--- fbs13/src/main/java/com/example/demo/UserInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInterface extends JpaRepository<User, String> {
-	public User findByEmail(String email);
-}
